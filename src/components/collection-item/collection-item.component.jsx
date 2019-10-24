3d41a20d3318e0fa2caa155b7f8a37a2fdedf605
Juan Huttemann <juanfhuttemann@gmail.com>
2019-10-24 12:22:29 -0300

    add collection-item styles

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './collection-item.styles.scss'
+import CustomButton from '../custom-button/custom-button.component';
 
 const CollectionItem = ({id, name, price, imageUrl})=> (
     <div className='collection-item'>
@@ -13,7 +14,8 @@ const CollectionItem = ({id, name, price, imageUrl})=> (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
+        <CustomButton inverted>Add to cart</CustomButton>
     </div>
 )
 
-export default CollectionItem
+export default CollectionItem;
